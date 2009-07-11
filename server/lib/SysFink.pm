eab5a0359d8f5542dc690082f5c703b23e6c3e3e
Michal Jurosz <mj@mj41.cz>
2009-07-11 10:46:58 +0000

    server - base module stubs

---
+++ server/lib/SysFink.pm
@@ -0,0 +1,43 @@
+package SysFink;
+
+our $VERSION = '0.10';
+
+=head1 NAME
+
+SysFink - The unix based guard for checking consistency of system files.
+
+=head1 DESCRIPTION
+
+The unix based guard for checking consistency of system files. Sysfink periodically scan basic information in the file system and reports changes to administrator.
+
+=head1 AUTHORS
+
+Tomáš Podermañski <tpoder@cis.vutbr.cz>
+Michal Jurosz <mj@mj41.cz>
+
+=head1 LICENSE
+
+SysFink is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+SysFink is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Foobar. If not, see <http://www.gnu.org/licenses/>.
+
+=head1 BUGS
+
+L<http://code.google.com/p/sysfink/issues/list>
+
+=head1 SEE ALSO
+
+L<SysFink::Server>, L<SysFink::Client>
+
+=cut
+
+1;
---
+++ server/lib/SysFink/Server.pm
@@ -0,0 +1,30 @@
+package SysFink::Server;
+
+use strict;
+use warnings;
+
+
+=head1 NAME
+
+SysFink::Server - SysFink server.
+
+=head1 SYNOPSIS
+
+See L<SysFink::Server>
+
+=head1 DESCRIPTION
+
+SysFink server.
+
+=head1 SEE ALSO
+
+L<SysFink>
+
+=head1 LICENSE
+
+This file is part of SysFink. See L<SysFink> license.
+
+=cut
+
+
+1;
