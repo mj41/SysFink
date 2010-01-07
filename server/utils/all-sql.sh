c86e1ff6624269cd3a7a880eb9f965367eb81f25
Michal Jurosz <mj@mj41.cz>
2010-01-07 20:30:49 +0000

    fix all-sql.sh a bit

--- server/utils/all-sql.sh
+++ server/utils/all-sql.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-echo -n "updating some temp/*.sql - "
+echo -n "Updating some temp/*.sql - "
 perl utils/wiki_schema.pl sql/schema.wiki > temp/schema.sql
 
-echo -n "creating temp/schema-raw-create.sql - "
+echo -n "Creating temp/schema-raw-create.sql - "
 perl utils/wiki_schema.pl sql/schema.wiki 0 1 0 > temp/schema-raw-create.sql && echo done
 
-echo -n "creating temp/schema-raw-create-comments.sql - "
+echo -n "Creating temp/schema-raw-create-comments.sql - "
 perl utils/wiki_schema.pl sql/schema.wiki 0 1 1 > temp/schema-raw-create-comments.sql && echo done
 
 echo "Transforming MySQL to SQLite (temp/schema-raw-create.sql > temp/schema-raw-create-sqlite.sql) :"
@@ -14,12 +14,14 @@ sqlt --parser MySQL --producer SQLite temp/schema-raw-create.sql > temp/schema-r
 
 
 if [ "$1" = "1" -o "$1" = "2" ]; then
-    echo -n "updating SysFink::DB::Schema.pm - "
+    echo -n "Updating SysFink::DB::Schema.pm - "
     perl utils/sqlt-sysfink.pl dbix temp/schema-raw-create.sql 0 && echo done
 fi
 
 
 if [ "$1" = "2" ]; then
-    echo -n "updating temp/schema.png - "
-    perl utils/sqlt-sysfink.pl dbdoc temp/schema-raw-create-comments.sql 0 && echo done
+    echo "Updating temp/dbdoc (schema images)"
+    perl utils/sqlt-sysfink.pl dbdoc temp/schema-raw-create-comments.sql 3 && echo done
 fi
+
+echo "Done."
