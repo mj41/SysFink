6132e94532c0d71d4696dd1d113b4216ef6ab8c4
Michal Jurosz <mj@mj41.cz>
2010-01-20 15:54:03 +0000

    server - table browser add some cols_in_foreign_tables

--- server/lib/SysFink/DB/SchemaAdd.pm
+++ server/lib/SysFink/DB/SchemaAdd.pm
@@ -4,9 +4,24 @@ use base 'SysFink::DB::Schema';
 
 # ViewMD - view metadata
 
-package SysFink::DB::Schema::machine;
-__PACKAGE__->restricted_cols( { 'passwd' => 1, 'ip' => 1, } );
+package SysFink::DB::Schema::aud;
+__PACKAGE__->cols_in_foreign_tables( [ qw/aud_id user_id date/ ] );
+
+package SysFink::DB::Schema::aud_status;
 __PACKAGE__->cols_in_foreign_tables( [ qw/name/ ] );
+
+package SysFink::DB::Schema::aud_status;
+__PACKAGE__->cols_in_foreign_tables( [ qw/name/ ] );
+
+package SysFink::DB::Schema::machine;
+__PACKAGE__->restricted_cols( { 'ip' => 1, } );
+__PACKAGE__->cols_in_foreign_tables( [ qw/name/ ] );
+
+package SysFink::DB::Schema::mconf_sec;
+__PACKAGE__->cols_in_foreign_tables( [ qw/mconf_sec_id name mconf_id/ ] );
+
+package SysFink::DB::Schema::path;
+__PACKAGE__->cols_in_foreign_tables( [ qw/path/ ] );
 
 package SysFink::DB::Schema::user;
 __PACKAGE__->restricted_cols( { 'passwd' => 1, } );
